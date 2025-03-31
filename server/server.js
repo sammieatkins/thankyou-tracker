3511c5bc63812ff616a8b8f11bcf3fdf6a6440f4
sammieatkins <atk21007@byui.edu>
2025-03-31 00:12:57 -0600

    new attempt

--- server/server.js
+++ server/server.js
@@ -6,33 +6,34 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
+// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("🎉 Server is up and running!");
 });
 
-// Routes
+// API ROUTES
 const giftRoutes = require("./routes/gifts");
 app.use("/api/gifts", giftRoutes);
 
-// DB Connection
+// DB CONNECTION
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected!");
+
     const path = require("path");
 
+    // SERVE FRONTEND STATIC FILES
     app.use(express.static(path.join(__dirname, "../client/dist/client")));
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "../client/dist/client/index.html"));
     });
 
+    // START SERVER
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error(err));
